81e8edb20d9842568f1a1eb87cd869197deb0098
Jesse Gallagher <jesse@secondfoundation.org>
2015-04-14 22:21:49 -0400

    Moved settings controls to the platform

--- darwino-demo/parent-dwo-demo-news/dwo-demo-news-robovm-native/src/com/darwino/mobile/platform/settings/AboutSettingsPane.java
+++
@@ -1,5 +0,0 @@
-package com.darwino.mobile.platform.settings;
-
-public interface AboutSettingsPane extends SettingsPane {
-
-}
--- darwino-demo/parent-dwo-demo-news/dwo-demo-news-robovm-native/src/com/darwino/mobile/platform/settings/SettingsRoot.java
+++
@@ -1,5 +0,0 @@
-package com.darwino.mobile.platform.settings;
-
-public interface SettingsRoot {
-	public void save();
-}
