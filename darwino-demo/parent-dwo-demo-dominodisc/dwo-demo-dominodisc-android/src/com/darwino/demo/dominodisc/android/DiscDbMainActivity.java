479ee0e6de6fbec37f46a7b7b7ff966848113a4c
priand <phil@triloggroup.com>
2015-04-21 18:27:50 -0400

    Adapted the code to the new task factory

--- darwino-demo/parent-dwo-demo-dominodisc/dwo-demo-dominodisc-android/src/com/darwino/demo/dominodisc/android/DiscDbMainActivity.java
+++ darwino-demo/parent-dwo-demo-dominodisc/dwo-demo-dominodisc-android/src/com/darwino/demo/dominodisc/android/DiscDbMainActivity.java
@@ -11,37 +11,13 @@
 
 package com.darwino.demo.dominodisc.android;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-import com.darwino.android.platform.hybrid.DarwinoAndroidHybridActions;
 import com.darwino.android.platform.hybrid.DarwinoCordovaHybridActivity;
-import com.darwino.android.platform.hybrid.HybridJavaScriptInterface;
-import com.darwino.mobile.platform.hybrid.MobileHybridRestFactory;
 
 public class DiscDbMainActivity extends DarwinoCordovaHybridActivity {
 	
-	private class DiscDbTasks extends DarwinoAndroidHybridActions {
-		DiscDbTasks(Activity activity) {
-			super(activity);
-		}
-		@Override
-		public void refreshUi() {
-			loadMainPage();
-	    }
-	}
-
 	public DiscDbMainActivity() {
-		// TEMP here...
-		MobileHybridRestFactory.hybridTasks = getCommonTasks(); 
-	}
-	
-	@Override
-	public Object getDawinoJSInterface() {
-		return new HybridJavaScriptInterface(getCommonTasks());
-	}
-	private DiscDbTasks getCommonTasks() {
-		return new DiscDbTasks(getActivity());
 	}
 	
 	@Override
@@ -56,34 +32,4 @@ public class DiscDbMainActivity extends DarwinoCordovaHybridActivity {
         
         loadMainPage();
 	}
-
-/*	
-	@TargetApi(Build.VERSION_CODES.KITKAT)
-	@Override
-    public void init(CordovaWebView webView, CordovaWebViewClient webViewClient, CordovaChromeClient webChromeClient) {
-		super.init(webView, webViewClient, webChromeClient);
-		
-		//WebSettings settings =  webView.getSettings();
-		//settings.setAllowUniversalAccessFromFileURLs(true);
-		
-//		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//		    if(true || (getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE )!=0 ) {
-//		        //webView.setWebContentsDebuggingEnabled(true);
-//		    	try {
-//			    	Method m = CordovaWebViewClient.class.getMethod("setWebContentsDebuggingEnabled", Boolean.TYPE);
-//			    	m.invoke(webView, true);
-//		    	} catch(Exception ex) {
-//		    		ex.printStackTrace();
-//		    	}
-//		    }
-//		}
-		
-		// Enable debugging for KitKat
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-		    if(true || (getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE )!=0 ) {
-		        WebView.setWebContentsDebuggingEnabled(true);
-		    }
-		}    
-	}
-*/
 }
