f80aa32645ad03071c5120d8f100a5f4d07075de
priand <phil@triloggroup.com>
2015-06-20 09:38:35 -0400

    Update AppServlet with app related services

--- darwino-demo/dwo-playground/dwo-playground-domino/src/main/java/com/darwino/playground/domino/AppServlet.java
+++ darwino-demo/dwo-playground/dwo-playground-domino/src/main/java/com/darwino/playground/domino/AppServlet.java
@@ -14,8 +14,10 @@ package com.darwino.playground.domino;
 import javax.servlet.ServletContext;
 
 import com.darwino.commons.json.JsonException;
+import com.darwino.commons.services.HttpServiceFactories;
 import com.darwino.domino.application.DarwinoApplicationServlet;
 import com.darwino.domino.application.DarwinoDominoApplication;
+import com.darwino.playground.app.AppServiceFactory;
 
 
 
@@ -34,5 +36,14 @@ public class AppServlet extends DarwinoApplicationServlet {
 	@Override
 	protected DarwinoDominoApplication createDarwinoApplication(ServletContext servletContext) throws JsonException {
 		return DarwinoApplication.create(servletContext);
+	}
+	
+	/**
+	 * Add the application specific services. 
+	 */
+	@Override
+	protected void addApplicationServiceFactories(HttpServiceFactories factories) {
+		// The service should always executed locally when running on a server
+		factories.add(new AppServiceFactory());
 	}
 }
