e172f0aaa7def3903600e4b562ea7a54fd4bf11c
priand <phil@triloggroup.com>
2015-07-05 18:36:28 -0400

    adapt to the bean API

--- darwino-demo/parent-dwo-demo-dominodisc/dwo-demo-dominodisc-j2ee/src/main/java/com/darwino/demo/dominodisc/web/DiscDbJ2EEApplication.java
+++ darwino-demo/parent-dwo-demo-dominodisc/dwo-demo-dominodisc-j2ee/src/main/java/com/darwino/demo/dominodisc/web/DiscDbJ2EEApplication.java
@@ -13,7 +13,7 @@ package com.darwino.demo.dominodisc.web;
 
 import javax.servlet.ServletContext;
 
-import com.darwino.config.jsonstore.JsonDb;
+import com.darwino.commons.platform.ManagedBeansService;
 import com.darwino.j2ee.application.DarwinoJ2EEApplication;
 import com.darwino.platform.DarwinoManifest;
 
@@ -29,7 +29,7 @@ public class DiscDbJ2EEApplication extends DarwinoJ2EEApplication {
 	}
 	
 	@Override
-	public String[] getConfigurationBeanNames(String type) {
-		return new String[] {"discdb","demo",JsonDb.BEAN_LOCAL_NAME,JsonDb.BEAN_DEFAULT_NAME};
+	public String[] getConfigurationBeanNames() {
+		return new String[] {"discdb","demo",ManagedBeansService.LOCAL_NAME,ManagedBeansService.DEFAULT_NAME};
 	}
 }
