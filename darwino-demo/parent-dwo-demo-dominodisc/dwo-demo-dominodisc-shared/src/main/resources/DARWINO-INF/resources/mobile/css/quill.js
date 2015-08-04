e13db9a16aeb8ceb5ac5b0e1ce6a4abfdea1964b
Jesse Gallagher <jesse@secondfoundation.org>
2015-08-04 17:01:45 -0400

    Re-added Material design to get new-post button to appear and added Quill rich text editor

---
+++ darwino-demo/parent-dwo-demo-dominodisc/dwo-demo-dominodisc-shared/src/main/resources/DARWINO-INF/resources/mobile/css/quill.js
@@ -0,0 +1,8 @@
+.selectable {
+  -webkit-user-select: auto !important;
+  -khtml-user-select: auto !important;
+  -moz-user-select: auto !important;
+  -ms-user-select: auto !important;
+  -o-user-select: auto !important;
+  user-select: auto !important;
+}
