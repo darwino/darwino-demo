30a9f5615ac048c541f60d7bcf8e16d8b54cab24
priand <phil@triloggroup.com>
2016-01-12 18:23:58 -0500

    Use UUID generator

--- darwino-demo/dwo-demo-dominodisc/dwo-demo-dominodisc-j2ee/src/main/java/com/darwino/demo/dominodisc/app/AppDDLGenerator.java
+++ darwino-demo/dwo-demo-dominodisc/dwo-demo-dominodisc-j2ee/src/main/java/com/darwino/demo/dominodisc/app/AppDDLGenerator.java
@@ -11,8 +11,8 @@
 
 package com.darwino.demo.dominodisc.app;
 
+import com.darwino.commons.util.UUIDGenerator;
 import com.darwino.j2ee.jstore.JdbcDdlGenerator;
-import com.darwino.jsonstore.impl._Utils;
 import com.darwino.jsonstore.sql.impl.full.JdbcDatabaseCustomizer;
 import com.darwino.platform.DarwinoManifest;
 import com.darwino.sql.drivers.DBDriver;
@@ -44,7 +44,7 @@ public class AppDDLGenerator {
 			// Create the generator
 			// The replicaId should be kept the same when the database is upgraded and the replicated data should be kept
 			// A newly generated ID will actually restart the replication from scratch
-			String replicaId = _Utils.uuid();
+			String replicaId = UUIDGenerator.uuid();
 			JdbcDdlGenerator gen = new JdbcDdlGenerator(dbDriver, dbSchema, mf.getDatabaseFactory(), customizer, replicaId);
 
 			// Generate the DDL for all the databases to System.out (Java console)
