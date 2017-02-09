3fc726bae701aab8fe82fe1e9cd27d669f052f08
priand <phil@triloggroup.com>
2017-02-08 22:20:28 -0500

    Better authentication error reporting

--- darwino-demo/dwo-demo-dominodisc/dwo-demo-dominodisc-webui/src/main/resources/DARWINO-INF/resources/mobile/js/darwino.angular.jstore.js
+++ darwino-demo/dwo-demo-dominodisc/dwo-demo-dominodisc-webui/src/main/resources/DARWINO-INF/resources/mobile/js/darwino.angular.jstore.js
@@ -578,6 +578,7 @@ darwino.provide("darwino/angular/jstore",null,function() {
 	            // Session requires authentication
 	            if (status==401 || status==419 || response.headers('x-dwo-auth-msg')=='authrequired' ){
 	            	interceptor.unauthorized(response);
+		            return $q.reject(response);
 	            } else if(status<200 || status>299) {
 		            return $q.reject(response);
 	            }
