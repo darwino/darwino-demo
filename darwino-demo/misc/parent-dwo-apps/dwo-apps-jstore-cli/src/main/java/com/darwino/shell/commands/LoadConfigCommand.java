6b1cb1766bfcda7653a141ffa0e04bc3194aa474
Jesse Gallagher <jesse@secondfoundation.org>
2018-05-25 17:24:02 -0400

    Add contacts-react domino project and update license headers

--- darwino-demo/misc/parent-dwo-apps/dwo-apps-jstore-cli/src/main/java/com/darwino/shell/commands/LoadConfigCommand.java
+++ darwino-demo/misc/parent-dwo-apps/dwo-apps-jstore-cli/src/main/java/com/darwino/shell/commands/LoadConfigCommand.java
@@ -1,14 +1,23 @@
-/*!COPYRIGHT HEADER! - CONFIDENTIAL 
+/*!COPYRIGHT HEADER! 
  *
- * Darwino Inc Confidential.
+ * (c) Copyright Darwino Inc. 2014-2018.
  *
- * (c) Copyright Darwino Inc. 2014-2016.
+ * Licensed under The MIT License (https://opensource.org/licenses/MIT)
  *
- * Notice: The information contained in the source code for these files is the property 
- * of Darwino Inc. which, with its licensors, if any, owns all the intellectual property 
- * rights, including all copyright rights thereto.  Such information may only be used 
- * for debugging, troubleshooting and informational purposes.  All other uses of this information, 
- * including any production or commercial uses, are prohibited. 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.darwino.shell.commands;
